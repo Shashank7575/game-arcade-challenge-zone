6ef8e3b28ce3a336a142a2d19b5d3bb7497ff1a6
Shashank7575 <shashankchaudhary7575@gmail.com>
2025-06-14 15:58:21 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,8 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/game-arcade-challenge-zone/',                         
   server: {
     host: "::",
     port: 8080,
